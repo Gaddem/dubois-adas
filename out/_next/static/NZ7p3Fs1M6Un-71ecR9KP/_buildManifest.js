074797574178cdfdca4e9393a1502a1d959ff796
lmk-kevin <kevin@lmksystem.fr>
2025-10-21 17:53:24 +0200

    Build static export

---
+++ out/_next/static/NZ7p3Fs1M6Un-71ecR9KP/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/59c555b18e1b820c.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ out/_next/static/chunks/22aa305bd70cd64c.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/439bf150647006e2.js",
+  "static/chunks/turbopack-df325e34df5349ac.js"
+])
