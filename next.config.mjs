b58118eedf27f620d342dd7f9ec9f88060cc505f
lmk-kevin <kevin@lmksystem.fr>
2025-10-21 17:50:56 +0200

    Build static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true }, // utile si tu utilises next/image
+  basePath: '/dubois-adas', // <-- change "mon-site" par le nom de ton repo
+};
 
 export default nextConfig;
